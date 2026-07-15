1a7bb6f26108010c76d395b7acd45157a86c11a7
gungun88 <pengqibao3@gmail.com>
2026-07-15 13:29:30 +0800

    Fix red packet admin settings binding

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,36 +1,53 @@
 import app from 'flarum/admin/app';
+import Switch from 'flarum/common/components/Switch';
 
 app.initializers.add('doingfb-red-packet-admin', () => {
   app.extensionData
     .for('doingfb-red-packet')
-    .registerSetting({
-      setting: 'doingfb-red-packet.enabled',
-      type: 'boolean',
-      label: app.translator.trans('doingfb-red-packet.admin.settings.enabled'),
+    .registerSetting(function () {
+      return (
+        <div className="Form-group">
+          <Switch state={this.setting('doingfb-red-packet.enabled')() !== '0'} onchange={(value) => this.setting('doingfb-red-packet.enabled')(value ? '1' : '0')}>
+            {app.translator.trans('doingfb-red-packet.admin.settings.enabled')}
+          </Switch>
+        </div>
+      );
     })
-    .registerSetting({
-      setting: 'doingfb-red-packet.min_amount',
-      type: 'number',
-      label: app.translator.trans('doingfb-red-packet.admin.settings.min_amount'),
-      help: app.translator.trans('doingfb-red-packet.admin.settings.min_amount_help'),
+    .registerSetting(function () {
+      return (
+        <div className="Form-group">
+          <label>{app.translator.trans('doingfb-red-packet.admin.settings.min_amount')}</label>
+          <div className="helpText">{app.translator.trans('doingfb-red-packet.admin.settings.min_amount_help')}</div>
+          <input className="FormControl" type="number" min="0.01" step="0.0001" bidi={this.setting('doingfb-red-packet.min_amount')} />
+        </div>
+      );
     })
-    .registerSetting({
-      setting: 'doingfb-red-packet.max_amount',
-      type: 'number',
-      label: app.translator.trans('doingfb-red-packet.admin.settings.max_amount'),
-      help: app.translator.trans('doingfb-red-packet.admin.settings.max_amount_help'),
+    .registerSetting(function () {
+      return (
+        <div className="Form-group">
+          <label>{app.translator.trans('doingfb-red-packet.admin.settings.max_amount')}</label>
+          <div className="helpText">{app.translator.trans('doingfb-red-packet.admin.settings.max_amount_help')}</div>
+          <input className="FormControl" type="number" min="0.01" step="0.0001" bidi={this.setting('doingfb-red-packet.max_amount')} />
+        </div>
+      );
     })
-    .registerSetting({
-      setting: 'doingfb-red-packet.max_count',
-      type: 'number',
-      label: app.translator.trans('doingfb-red-packet.admin.settings.max_count'),
-      help: app.translator.trans('doingfb-red-packet.admin.settings.max_count_help'),
+    .registerSetting(function () {
+      return (
+        <div className="Form-group">
+          <label>{app.translator.trans('doingfb-red-packet.admin.settings.max_count')}</label>
+          <div className="helpText">{app.translator.trans('doingfb-red-packet.admin.settings.max_count_help')}</div>
+          <input className="FormControl" type="number" min="1" max="500" step="1" bidi={this.setting('doingfb-red-packet.max_count')} />
+        </div>
+      );
     })
-    .registerSetting({
-      setting: 'doingfb-red-packet.expires_minutes',
-      type: 'number',
-      label: app.translator.trans('doingfb-red-packet.admin.settings.expires_minutes'),
-      help: app.translator.trans('doingfb-red-packet.admin.settings.expires_minutes_help'),
+    .registerSetting(function () {
+      return (
+        <div className="Form-group">
+          <label>{app.translator.trans('doingfb-red-packet.admin.settings.expires_minutes')}</label>
+          <div className="helpText">{app.translator.trans('doingfb-red-packet.admin.settings.expires_minutes_help')}</div>
+          <input className="FormControl" type="number" min="1" max="10080" step="1" bidi={this.setting('doingfb-red-packet.expires_minutes')} />
+        </div>
+      );
     })
     .registerPermission({
       icon: 'fas fa-envelope-open-text',
